b3891d6d6a069d75b5d4ceebf74e712ea6a0dd4a
Michael Symmes <msymmes@gmail.com>
2018-08-28 14:53:35 -0400

    Initial Commit

---
+++ app/containers/HomePage/HomePage.js
@@ -0,0 +1,32 @@
+/*
+ * HomePage
+ *
+ * This is the first thing users see of our App, at the '/' route
+ */
+
+import React from 'react';
+// import PropTypes from 'prop-types';
+import { Helmet } from 'react-helmet';
+import './style.scss';
+
+export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <article>
+        <Helmet>
+          <title>Home Page</title>
+          <meta name="description" content="A React.js Boilerplate application homepage" />
+        </Helmet>
+        <div className="home-page">
+          <section className="centered">
+            <h2>Start your next react project in seconds</h2>
+            <p>A minimal <i>React-Redux</i> boilerplate with all the best practices</p>
+          </section>
+        </div>
+      </article>
+    );
+  }
+}
+
+HomePage.propTypes = {
+};

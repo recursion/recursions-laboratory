6b21a3e23b6ea649ab5e4abebc83a98eb5f0e27f
Michael Symmes <msymmes@gmail.com>
2018-08-29 14:16:58 -0400

    Updated intro and multiple styles.

--- app/components/Intro/Intro.js
+++ app/components/Intro/Intro.js
@@ -3,16 +3,33 @@ import BioPic from './images/selfie.png';
 import './style.scss';
 
 const Intro = () => (
-  <div className="container">
-    <div className="about-me is-clearfix">
-      <figure className="image is-128x128 is-pulled-right">
+  <div className="intro container">
+    <div className="is-clearfix">
+      <figure className="image is-pulled-right">
         <img className="is-rounded" src={BioPic} alt="Grinning Me" />
       </figure>
       <h5 className="title is-size-6-mobile">Michael Symmes</h5>
       <h6 className="subtitle is-size-7-mobile">Explorer. Tinker. Builder of things.</h6>
       <p>
-        A lifelong problem solver who loves building, learning and exploring technology of all types.
+        A lifelong problem solver who loves building,
+        learning and exploring technology of all types.
+        I&apos;ve been tinkering with computers since I was a kid in the mid 1980&apos;s,
+        Linux since the early-mid 1990&apos;s and the WWW almost since it first
+        became a protocol. When it comes to computing, I am definitely a Jack-of-all-trades
+        and a master at RTFM.
       </p>
+      <p>
+        While I have dabbled in many different programming
+        language&apos;s in my lifetime; during the last few
+        years I have been focused mostly on front end web-development:
+        building with JavaScript(ES6) and
+        Elm(<span role="img" aria-label="heart-emoji">❤️</span>). Elm has completely
+        opened my eyes to the power of statically typed, functional languages, and simple,
+        opinionated language design. I have found it to be an amazing answer
+        to JavaScript fatigue.
+      </p>
+      <p>If you want to read more about me, and my history in computing
+        explorations... check out my <a>about me</a> section.</p>
     </div>
   </div>
 );

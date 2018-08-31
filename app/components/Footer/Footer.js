331095251a3dbe57d12eb5223f315d0250942726
Michael Symmes <msymmes@gmail.com>
2018-08-31 09:41:00 -0400

    Cleaned up footer links.

--- app/components/Footer/Footer.js
+++ app/components/Footer/Footer.js
@@ -3,21 +3,35 @@ import { Link } from 'react-router-dom';
 import './style.scss';
 
 const Footer = () => (
-  <footer>
-    <div>Built by Michael Symmes using <a href="https://reactjs.org">React</a> and <a href="https://bulma.io">Bulma</a>.</div>
-    <div className="router-links columns">
-      <Link className="router-link column" to="/projects">
-        Projects
-      </Link>
-      <Link className="router-link column" to="/skills">
-        Skills
-      </Link>
-      <Link className="router-link column" to="/contact">
-        Contact
-      </Link>
-      <Link className="router-link column" to="/">
-        Home
-      </Link>
+  <footer className="section">
+    <div className="container">
+      <div className="has-text-centered">
+        Built by Michael Symmes using <a href="https://reactjs.org">React</a> and <a href="https://bulma.io">Bulma</a>.
+      </div>
+      <div className="columns">
+        <div className="router-links column">
+          <p>
+            <Link className="router-link " to="/projects">
+              Projects
+            </Link>
+          </p>
+          <p>
+            <Link className="router-link " to="/skills">
+              Skills
+            </Link>
+          </p>
+          <p>
+            <Link className="router-link " to="/contact">
+              Contact
+            </Link>
+          </p>
+          <p>
+            <Link className="router-link " to="/">
+              Home
+            </Link>
+          </p>
+        </div>
+      </div>
     </div>
   </footer>
 );

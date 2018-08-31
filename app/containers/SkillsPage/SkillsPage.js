51673b701f92dd799540ef5c8d4b43de32157723
Michael Symmes <msymmes@gmail.com>
2018-08-31 14:31:16 -0400

    Updates to layout, styling, and new skills added.

--- app/containers/SkillsPage/SkillsPage.js
+++ app/containers/SkillsPage/SkillsPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import SkillItem from 'components/SkillItem';
+import './style.scss';
 
 export default class SkillsPage extends React.PureComponent {
   componentWillMount() {
@@ -16,20 +17,20 @@ export default class SkillsPage extends React.PureComponent {
     }
 
     return (
-      <section className="section">
+      <section className="skills section">
         <h1 className="title has-text-centered">Skills (that pay the bills)</h1>
         <div className="container is-size-5-desktop">
           <p>
-            Rating yourself is not easy, but I&apos;ll give it a try.
-            By far my strongest abilities are in being able to jump around and make things
-            work quickly and effeciently. If I had to label myself based on skills, it would be something like
-            &apos;Intermediate Full-Stack Generalist&apos;.
+            Rating yourself is not easy, but I&apos;ll give it a try anyway.
+            By far, my strongest abilities are in being able to jump around and make things
+            work quickly and effeciently. If I had to label myself based on skills,
+            it would be something like <em>&apos;Intermediate Full-Stack Generalist&apos;</em>.
+            To be fair, I&apos;m probably on the low end of Intermediate, and the high end of Junior.
           </p>
           <p className="has-text-centered has-text-weight-semibold">
             On a scale of 1-5, with 1 being the lowest:
           </p>
         </div>
-        <hr />
         <div className="columns is-multiline">
           {
             this.props.skills.map((skill) => <SkillItem key={skill.name} {...skill} />)

c3d55565b1a5b4b30370b17c9709735d4582676f
Michael Symmes <msymmes@gmail.com>
2018-08-30 22:57:20 -0400

    Added simple validations.

--- app/containers/ContactPage/ContactPage.js
+++ app/containers/ContactPage/ContactPage.js
@@ -1,42 +1,155 @@
 import React from 'react';
 import './style.scss';
 
-const ContactPage = () => (
-  <section className="section contact-page">
-    <div className="container">
-      <p>
-        Interested in hiring me? Discussing some of my work? Or whatever...
-        Go ahead.. reach out!
-      </p>
-      <div className="field">
-        <label className="label" htmlFor="subject">Subject</label>
-        <div className="control">
-          <input id="subject" className="input" type="text" placeholder="e.g Alex Smith" />
-        </div>
-      </div>
+const validateEmail = (email) => {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
 
-      <div className="field">
-        <label className="label" htmlFor="email">Your Email</label>
-        <div className="control">
-          <input id="email" className="input" type="email" placeholder="e.g. alexsmith@gmail.com" />
-        </div>
-      </div>
-      <div className="field">
-        <label className="label" htmlFor="message">Message</label>
-        <div className="control">
-          <textarea id="message" className="textarea" placeholder="Textarea"></textarea>
-        </div>
-      </div>
-      <div className="field is-grouped">
-        <div className="control">
-          <button className="button is-link">Submit</button>
-        </div>
-        <div className="control">
-          <button className="button is-text">Cancel</button>
-        </div>
-      </div>
-    </div>
-  </section>
-);
+export default class ContactPage extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      submitted: false,
+      subject: '',
+      email: '',
+      message: '',
+      subjectHelp: '',
+      emailHelp: '',
+      messageHelp: ''
+    };
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onSubjectChange = this.onSubjectChange.bind(this);
+    this.onEmailChange = this.onEmailChange.bind(this);
+    this.onMessageChange = this.onMessageChange.bind(this);
+  }
 
-export default ContactPage;
+  onSubmit() {
+    // submit form data here.
+    if (this.state.subject === '') {
+      this.setState(() => ({ subjectHelp: 'Subject must not be empty.' }));
+      return;
+    }
+
+    if (this.state.email === '') {
+      this.setState(() => ({ emailHelp: 'Email must not be empty.' }));
+      return;
+    }
+
+    if (!validateEmail(this.state.email)) {
+      this.setState(() => ({ emailHelp: 'Invalid Email Address' }));
+      return;
+    }
+
+    if (this.state.message === '') {
+      this.setState(() => ({ messageHelp: 'Message must not be empty.' }));
+      return;
+    }
+    this.setState(() => ({ submitted: true }));
+  }
+
+  onSubjectChange(subject) {
+    if (subject !== '') {
+      this.setState(() => ({ subjectHelp: '' }));
+    }
+    this.setState(() => ({ subject }));
+  }
+
+  onEmailChange(email) {
+    if (email !== '') {
+      this.setState(() => ({ emailHelp: '' }));
+    }
+    this.setState(() => ({ email }));
+  }
+
+  onMessageChange(message) {
+    if (message !== '') {
+      this.setState(() => ({ messageHelp: '' }));
+    }
+    this.setState(() => ({ message }));
+  }
+
+  render() {
+    if (this.state.submitted) {
+      return (
+        <div className="submitted-message has-text-centered is-size-1-desktop">
+          <p>
+            Your message has been submitted.
+          </p>
+          <p>
+            Thank you!
+          </p>
+        </div>
+      );
+    }
+    return (
+      <section className="section contact-page">
+        <div className="container">
+          <p>
+            Interested in hiring me? Discussing some of my work? Or whatever...
+            Go ahead.. reach out!
+          </p>
+          <div className="field">
+            <label className="label" htmlFor="subject">Subject</label>
+            <div className="control">
+              <input
+                onChange={(e) => this.onSubjectChange(e.target.value)}
+                value={this.state.subject}
+                id="subject"
+                className={`input ${(this.state.subjectHelp) ? 'is-danger' : ''}`}
+                type="text"
+                placeholder="e.g Alex Smith"
+              />
+              {(this.state.subjectHelp !== '') ?
+                <p className="help is-danger">{this.state.subjectHelp}</p> :
+                ''
+              }
+            </div>
+          </div>
+
+          <div className="field">
+            <label className="label" htmlFor="email">Your Email</label>
+            <div className="control">
+              <input
+                onChange={(e) => this.onEmailChange(e.target.value)}
+                value={this.state.email}
+                id="email"
+                className={`input ${(this.state.emailHelp) ? 'is-danger' : ''}`}
+                type="email"
+                placeholder="e.g. alexsmith@gmail.com"
+              />
+              {(this.state.emailHelp !== '') ?
+                <p className="help is-danger">{this.state.emailHelp}</p> :
+                ''
+              }
+            </div>
+          </div>
+          <div className="field">
+            <label className="label" htmlFor="message">Message</label>
+            <div className="control">
+              <textarea
+                onChange={(e) => this.onMessageChange(e.target.value)}
+                value={this.state.message}
+                id="message"
+                className={`textarea ${(this.state.messageHelp) ? 'is-danger' : ''}`}
+                placeholder="Textarea"
+              />
+              {(this.state.messageHelp !== '') ?
+                <p className="help is-danger">{this.state.messageHelp}</p> :
+                ''
+              }
+            </div>
+          </div>
+          <div className="field is-grouped">
+            <div className="control">
+              <button className="button is-link" onClick={this.onSubmit}>Submit</button>
+            </div>
+            <div className="control">
+              <button className="button is-text">Cancel</button>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}

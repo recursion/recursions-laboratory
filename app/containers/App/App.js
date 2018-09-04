1b17d30e72de9047e4f7f97780ffba5a46a58101
Michael Symmes <msymmes@gmail.com>
2018-09-04 11:27:37 -0400

    Fixed react html prop.

--- app/containers/App/App.js
+++ app/containers/App/App.js
@@ -26,7 +26,7 @@ const App = () => (
       defaultTitle="In the lab."
     >
       <meta name="description" content="About me, and some of the things I've built." />
-      <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js" integrity="sha384-kW+oWsYx3YpxvjtZjFXqazFpA7UP/MbiY4jvs+RWZo2+N94PFZ36T6TFkc9O3qoB" crossorigin="anonymous" />
+      <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js" integrity="sha384-kW+oWsYx3YpxvjtZjFXqazFpA7UP/MbiY4jvs+RWZo2+N94PFZ36T6TFkc9O3qoB" crossOrigin="anonymous" />
     </Helmet>
     <Header />
     <Switch>

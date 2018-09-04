14ea2d0042e20a3a8c020b4240b525ac2f6f9557
Michael Symmes <msymmes@gmail.com>
2018-09-04 02:22:41 -0400

    Added icons and improved display.

--- app/components/SkillItem/SkillItem.js
+++ app/components/SkillItem/SkillItem.js
@@ -1,28 +1,67 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import colorPicker from './colorPicker';
 import './style.scss';
 
-const SkillItem = (skill) => {
-  if (skill.name !== '' && skill.level !== '') {
-    return (
-      <div className="column is-one-third is-flex">
-        <div className={`notification ${colorPicker()} is-bold`}>
-          <h1 className="title is-size-5">
-            {skill.name}
-          </h1>
-          <h2 className="subtitle is-size-6">
-            Skill Level: {skill.level}
-          </h2>
-          <div className="content is-flex">
-            <p>
-              {skill.description}
-            </p>
-          </div>
-        </div>
-      </div>
-    );
+export default class SkillItem extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = { expanded: false };
+    this.expand = this.expand.bind(this);
   }
-  return '';
-};
 
-export default SkillItem;
+  expand() {
+    this.setState(() => ({ expanded: !this.state.expanded }));
+  }
+
+  render() {
+    const { skill } = this.props;
+    if (!skill) return '';
+    if (skill.name !== '' && skill.level !== '') {
+      return (
+        <div className="skill-item column is-one-third is-flex">
+          <article className={`media notification ${colorPicker()} is-bold`}>
+            <div className="media-content">
+              <div className="content">
+                <h1 className="title is-size-5">
+                  <figure className="skill-icon is-inline is-marginless">
+                    {
+                      (skill.icon) ?
+                        <span className="icon">
+                          <i className={`${skill.icon}`}></i>
+                        </span> :
+                        ''
+                    }
+                  </figure>
+                  {skill.name}
+                </h1>
+                <h2 className="subtitle is-size-7">
+                  Skill Level: {skill.level}
+                </h2>
+                <hr />
+                <p>
+                  {(!this.state.expanded && skill.description.length > 145) ?
+                    `${skill.description.slice(0, 145)}...` :
+                    skill.description
+                  }
+                  <button className="is-small is-link is-inline" onClick={this.expand}>
+                    <em>
+                      { (!this.state.expanded) ?
+                        '(more)' : '(less)'
+                      }
+                    </em>
+                  </button>
+                </p>
+              </div>
+            </div>
+          </article>
+        </div>
+      );
+    }
+    return '';
+  }
+}
+
+SkillItem.propTypes = {
+  skill: PropTypes.object
+};

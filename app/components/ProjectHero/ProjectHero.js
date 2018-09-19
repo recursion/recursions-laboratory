f4f30901d6e016760d2fbbdf3b761e7f3739a0a0
Michael Symmes <msymmes@gmail.com>
2018-09-19 14:25:20 -0400

    Increased hero size

--- app/components/ProjectHero/ProjectHero.js
+++ app/components/ProjectHero/ProjectHero.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ProjectItem from 'components/ProjectItem';
+import './style.scss';
 
 export default class ProjectHero extends React.PureComponent {
   constructor(props) {
